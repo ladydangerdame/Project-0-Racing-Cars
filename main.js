fbe94d2a251d03028f2a3fccca457e4fc8a5d666
ladydangerdame <turbofemme@gmail.com>
2016-11-06 21:43:58 -0800

    Gave up on .animate and tried to make checkWinner work to no avail

--- main.js
+++ main.js
@@ -2,34 +2,46 @@ console.log('linked!');
 
 $(document).ready(function() {
 
+
+
+
   $(document).keydown(function (e) {
-          var keyCode = e.keyCode || e.which,
+          var keyCode = e.keyCode
           keys = {a: 65, w: 87};
 
           switch (keyCode) {
 
               case keys.a:
-                  $('#car1').animate({"right": "-=5px"}, "fast");
+                  $('#car1').css({"margin-left": "+=30px"}, "fast");
+              checkWinner1();
+              checkWinner2();
               break;
 
+
               case keys.w:
-                  $('#car2').animate({"left": "+=5px"}, "fast");
-              break;
+                  $('#car2').css({"margin-left": "+=30px"}, "fast");
+
+              checkWinner1();
+              checkWinner2();
+
+              function checkWinner1(){
+                  if ($('.player1').css('margin-left')==='1400px') {
+                    alert('Player1 wins!');
+
+                  }
+              }
+              function checkWinner2(){
+                  if ($('.player2').css('margin-left')==='1400px') {
+                    alert('Player2 wins!');
+
+                  }
+              }
           }
       });
   });
 
 
-function checkWinner(){
-    if ($('#car1').css('margin-left')==='1400px') {
-      $('.container-fluid').append('<img  src="http://www.dailyhaha.com/_pics/super_ugly_monkey.jpg" />');
-        alert('Apple wins!');
-        $('body').css('background-color', '#5122EA');
 
-    } else if ($('#car2').css('margin-left')==='400px')
-    alert('Windows wins!');
-
-}
 
 $('button').click(function() {
     location.reload();
